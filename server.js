00731b9f688ea79650e108d8591f377f28e362e0
dharan thaker <41338131+dharan1999@users.noreply.github.com>
2020-05-02 12:11:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 
 let app = express();
 const PORT = process.env.PORT || 80;
-app.use(express.static(path.join(__dirname,'public')));
-app.get('*',(req,res) => res.sendfile(path.join(__dirname = 'src/app.js')));
+app.use(express.static('src'));
 app.listen(PORT,'0.0.0.0', function(){
     console.log("Node app is running on port",PORT);
 });
